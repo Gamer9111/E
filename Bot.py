75d54a3de8e598863ff6dc733484eb59137df97c
KillMePlz911 <43588744+KillMePlz911@users.noreply.github.com>
2019-01-04 16:44:19 -0600

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -4,13 +4,22 @@ from discord.ext import commands
 import asyncio
 import time
 import random
-import os
 from discord import Game
 
 
 Client = discord.client
 client = commands.Bot(command_prefix = '/')
 Clientdiscord = discord.Client()
+
+
+@client.event
+async def on_ready():
+    await client.change_presence(game=Game(name=' I hate all of you '))
+    print('die') 
+
+
+@client.event
+async def on_message(message):
 
 
 
